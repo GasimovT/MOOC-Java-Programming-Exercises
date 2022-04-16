aa98683637ffbb6c8259189b7cca51064d4ca96d
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-04-15 22:20:25 -0400

    Add files via upload

---
+++ part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class AverageOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count=0;
+        int sum=0;
+        while (true) {
+            System.out.println("Give a number:");
+            int num= scanner.nextInt();
+
+            if (num==0){
+                break;
+            } else{
+                count=count+1;
+                sum=sum+num;
+                continue;
+
+
+            }
+        }
+        if (count>0) {
+            double average = (double)sum/count;
+            System.out.println("Average of the numbers: "+average);
+        }
+
+    }
+}
