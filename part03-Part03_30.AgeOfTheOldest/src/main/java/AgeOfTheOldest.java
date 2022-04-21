fe5f466c90e36c53071df476130063f9b3e45151
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-04-21 16:37:33 -0400

    Add files via upload

---
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class AgeOfTheOldest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] pieces;
+        int max = 0;
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            pieces = input.split(",");
+            if (Integer.valueOf(pieces[1]) > max) {
+                max = Integer.valueOf(pieces[1]);
+            }
+
+        }
+        System.out.println("Age of the oldest: " + max);
+
+
+    }
+}
