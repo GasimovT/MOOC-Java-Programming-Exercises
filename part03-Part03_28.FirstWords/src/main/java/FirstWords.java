5e5c1f576fff50456b51d57294846fe2855533fc
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-04-21 16:37:14 -0400

    Add files via upload

---
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class FirstWords {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] pieces;
+
+        while(true){
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            pieces = input.split(" ");
+
+            if(pieces.length!=0){
+                System.out.println(pieces[0]);
+
+            }
+        }
+
+
+    }
+}
