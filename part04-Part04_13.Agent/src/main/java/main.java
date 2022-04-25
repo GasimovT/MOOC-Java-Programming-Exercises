34549a9658e823f05ec1385c1d5a63d60a7c01eb
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-04-25 00:47:43 -0400

    Add files via upload

---
+++ part04-Part04_13.Agent/src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Agent jack = new Agent("Jack","Sparrow");
+        System.out.println(jack.toString());
+        System.out.println(jack);
+    }
+}
