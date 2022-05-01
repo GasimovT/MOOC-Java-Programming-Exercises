8fddaf7edc8eba95268116766cd053302959847f
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-05-01 19:36:55 -0400

    Add files via upload

---
+++ part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -0,0 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GuestListFromAFile {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Name of the file:");
+        String file = scanner.nextLine();
+
+        ArrayList<String> list = new ArrayList<>();
+        // implement reading the file here.
+        try {
+            File file1 = new File(file);
+
+            BufferedReader bf = new BufferedReader(new FileReader(file1));
+            String st;
+            while ((st = bf.readLine()) != null) {
+                list.add(st);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Exception occurred: " + e.getMessage());
+        }
+        System.out.println("");
+
+        System.out.println("Enter names, an empty line quits.");
+        while (true) {
+            String name = scanner.nextLine();
+            if (name.isEmpty()) {
+                break;
+            }
+
+            if (list.contains(name)) {
+                System.out.println("The name is on the list.");
+            } else {
+                System.out.println("The name is not on the list.");
+            }
+        }
+
+        System.out.println("Thank you!");
+    }
+}
