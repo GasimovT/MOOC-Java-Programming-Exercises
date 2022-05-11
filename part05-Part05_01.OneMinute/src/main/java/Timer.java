ce406116003d2986f0d0880511050f7f0bd2ee25
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-05-11 18:02:27 -0400

    Add files via upload

---
+++ part05-Part05_01.OneMinute/src/main/java/Timer.java
@@ -0,0 +1,23 @@
+
+public class Timer {
+    private final ClockHand seconds;
+    private final ClockHand milliSeconds;
+
+
+    public Timer() {
+        this.seconds=new ClockHand(60);
+        this.milliSeconds= new ClockHand(100);
+    }
+
+    public void advance() {
+        this.milliSeconds.advance();
+        if(this.milliSeconds.value()==0){
+            this.seconds.advance();
+        }
+
+    }
+
+    public String toString() {
+        return this.seconds+":"+this.milliSeconds;
+    }
+}
