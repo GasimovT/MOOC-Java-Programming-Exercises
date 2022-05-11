4e448202abb0cdf24c40467296b127e191d28ac0
Tofig Gasimov <99315612+GasimovT@users.noreply.github.com>
2022-05-11 18:02:46 -0400

    Add files via upload

---
+++ part05-Part05_03.Cube/src/main/java/Program.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Experiment with your program here
+        Cube n = new Cube(8);
+        System.out.println(n);
+    }
+}
